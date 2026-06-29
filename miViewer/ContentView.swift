f060764a12506eebfa001f94f141d6e7ec79f4ec
metin-aksu <drmetin@msn.com>
2026-06-30 02:06:53 +0300

    Initial Commit

---
+++ miViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  miViewer
+//
+//  Created by Metin AKSU on 30.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ miViewer/miViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  miViewerApp.swift
+//  miViewer
+//
+//  Created by Metin AKSU on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct miViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
